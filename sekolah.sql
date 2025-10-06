f2b5faaa0caca2d6c51360ac5d5aee0e4de48ad1
sofiaaa05 <sofianugraheni05@gmail.com>
2025-10-06 09:02:24 +0700

    Memasukkan data siswa dan data nilai

--- sekolah.sql
+++ sekolah.sql
@@ -14,3 +14,23 @@ CREATE TABLE nilai (
   nilai INT
 );
 
+-- 2. MEMASUKKAN DATA SISWA
+
+INSERT INTO siswa (nama, umur, jurusan) VALUES
+('Andi', 16, 'IPA'),
+('Budi', 17, 'IPS'),
+('Citra', 16, 'IPA'),
+('Dewi', 17, 'Bahasa'),
+('Eko', 16, 'IPS');
+
+
+-- 3. MEMASUKKAN DATA NILAI
+
+INSERT INTO nilai (siswa_id, mata_pelajaran, nilai) VALUES
+(1, 'Matematika', 85),
+(1, 'Fisika', 90),
+(2, 'Bahasa Inggris', 88),
+(3, 'Biologi', 92),
+(4, 'Sastra', 80),
+(5, 'Ekonomi', 75);
+
